d7fb35d1e603deaded6ab7ce65bbb1b6712fff85
Safak TURKELI <sfktrkl@gmail.com>
2025-03-30 01:57:52 +0300

    Remove HelloWorld component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,13 @@
 <template>
   <GameHeader />
-  <div class="main-content">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
 import GameHeader from "./components/GameHeader.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
     GameHeader,
   },
 };
@@ -26,10 +20,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-.main-content {
-  margin-top: 80px;
-  padding: 20px;
 }
 </style>
