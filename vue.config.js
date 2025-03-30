059a6abcab79075975b16e0ae8c859c21e45f59d
Safak TURKELI <sfktrkl@gmail.com>
2025-03-30 21:20:50 +0300

    Configure webpack

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
+const webpack = require("webpack");
 
 module.exports = defineConfig({
   transpileDependencies: true,
@@ -7,5 +8,14 @@ module.exports = defineConfig({
       entry: "src/main.js",
       title: "Path of Vibe Coder",
     },
+  },
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: "false",
+        __VUE_PROD_DEVTOOLS__: false,
+        __VUE_OPTIONS_API__: true,
+      }),
+    ],
   },
 });
