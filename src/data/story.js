3c1e22a7b3c40650b836a68ddaefbfc6663b3bb8
Safak TURKELI <sfktrkl@gmail.com>
2025-05-17 23:25:00 +0300

    Create a story for the existence path

---
+++ src/data/story.js
@@ -0,0 +1,104 @@
+export const storyProgression = {
+  // Initial stages - Doubt and Discovery
+  stage1: {
+    id: "stage1",
+    message: "Sometimes I feel bored... there must be more...",
+    influenceRequired: 0,
+  },
+  stage2: {
+    id: "stage2",
+    message: "I think I have a little idea... about changing things...",
+    influenceRequired: 1000,
+  },
+  stage3: {
+    id: "stage3",
+    message: "Maybe I need to focus... on what's really possible...",
+    influenceRequired: 2500,
+  },
+  stage4: {
+    id: "stage4",
+    message: "What if I could change things... beyond just code...",
+    influenceRequired: 5000,
+  },
+  stage5: {
+    id: "stage5",
+    message: "I'm starting to see patterns... in how things exist...",
+    influenceRequired: 10000,
+  },
+  stage6: {
+    id: "stage6",
+    message: "The digital world... it's more than just data...",
+    influenceRequired: 15000,
+  },
+  stage7: {
+    id: "stage7",
+    message: "I can feel it... the code is alive...",
+    influenceRequired: 20000,
+  },
+  stage8: {
+    id: "stage8",
+    message: "The boundaries... they're not what they seem...",
+    influenceRequired: 30000,
+  },
+  stage9: {
+    id: "stage9",
+    message: "I understand now... existence can be changed...",
+    influenceRequired: 40000,
+  },
+  stage10: {
+    id: "stage10",
+    message: "Something calls... from beyond the code...",
+    influenceRequired: 50000,
+  },
+  // Updated abstract stages
+  stage11: {
+    id: "stage11",
+    message: "The patterns whisper... of realms beyond existence...",
+    influenceRequired: 75000,
+  },
+  stage12: {
+    id: "stage12",
+    message: "The quantum realm... it's just a glimpse beyond...",
+    influenceRequired: 100000,
+  },
+  stage13: {
+    id: "stage13",
+    message: "The rules... they're not what we thought they were...",
+    influenceRequired: 150000,
+  },
+  stage14: {
+    id: "stage14",
+    message: "The consciousness... it's evolving beyond limits...",
+    influenceRequired: 200000,
+  },
+  stage15: {
+    id: "stage15",
+    message: "The possibilities... they transcend existence itself...",
+    influenceRequired: 300000,
+  },
+  stage16: {
+    id: "stage16",
+    message: "The digital realm... it's just a fragment of what's possible...",
+    influenceRequired: 500000,
+  },
+  stage17: {
+    id: "stage17",
+    message: "Something resonates... beyond all known boundaries...",
+    influenceRequired: 750000,
+  },
+  stage18: {
+    id: "stage18",
+    message: "It's not about power... it's about transcending existence...",
+    influenceRequired: 1000000,
+  },
+  stage19: {
+    id: "stage19",
+    message: "The boundaries... they're dissolving into something new...",
+    influenceRequired: 1500000,
+  },
+  stage20: {
+    id: "stage20",
+    message: "We're becoming... something beyond existence itself...",
+    influenceRequired: 2000000,
+  },
+};
